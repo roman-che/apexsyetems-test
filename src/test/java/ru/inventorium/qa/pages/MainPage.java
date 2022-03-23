ec3daa84ef715a0ba1c73f482b33ae4489673e43
Roman Chernyavsky <56552851+roman-che@users.noreply.github.com>
2022-03-23 12:33:35 +0300

    added steps

--- src/test/java/ru/inventorium/qa/pages/MainPage.java
+++ src/test/java/ru/inventorium/qa/pages/MainPage.java
@@ -2,6 +2,7 @@ package ru.inventorium.qa.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.impl.SelenideElementListIterator;
+import io.qameta.allure.Step;
 import ru.inventorium.qa.helpers.DriverUtils;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -26,44 +27,53 @@ public class MainPage {
             pageFooterLocator = $("#block-eeocstatement");
 
     //actions
+    @Step("Open url \"https://www.apexsystems.com/\"")
     public void openMainPage() {
         open("https://www.apexsystems.com/");
     }
 
+    @Step("click \"accept cookies\"")
     public void clickAcceptCookies() {
         acceptCookiesButtonLocator.shouldBe(visible);
         acceptCookiesButtonLocator.click();
     }
 
+    @Step("menu elements of class toggler are not null")
     public void togglerMenuElementsArePresent() {
         mainNavigationLocator.shouldBe(visible);
         mainNavigationLocator.$$(".toggler").shouldBe(sizeGreaterThan(0));
     }
 
+    @Step("click \"What We Do\" menu item")
     public void clickWhatWeDoMenuItem() {
         whatWeDoMenuItemLocator.shouldBe(visible);
         whatWeDoMenuItemLocator.click();
     }
 
+    @Step("footer contains \"{FOOTER_TEXT}\"")
     public void assertFooterText() {
         pageFooterLocator.scrollTo();
         pageFooterLocator.shouldHave(matchText(FOOTER_TEXT));
     }
 
+    @Step("Page title should have text \"{TITLE_TEXT}\"")
     public void assertTitleText(String titleValue) {
         mainNavigationLocator.shouldBe(visible);
         assertThat(TITLE_TEXT).isEqualTo(titleValue);
     }
 
+    @Step("Console logs should not contain text 'SEVERE'")
     public void assertConsoleHaveNoErrors() {
         mainNavigationLocator.shouldBe(visible);
         String consoleLogs = DriverUtils.getConsoleLogs();
         assertThat(consoleLogs).doesNotContain(CONSOLE_ERROR_TEXT);
     }
 
+    @Step("Console logs should not contain text 'WARNING'")
     public void assertConsoleHaveNoWarnings() {
         mainNavigationLocator.shouldBe(visible);
         String consoleLogs = DriverUtils.getConsoleLogs();
         assertThat(consoleLogs).doesNotContain(CONSOLE_WARNING_TEXT);
     }
+
 }
--- src/test/java/ru/inventorium/qa/tests/ApexSystemsTests.java
+++ src/test/java/ru/inventorium/qa/tests/ApexSystemsTests.java
@@ -2,10 +2,8 @@ package ru.inventorium.qa.tests;
 
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.*;
-
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
-
 
 public class ApexSystemsTests extends TestBase {
 
@@ -14,32 +12,20 @@ public class ApexSystemsTests extends TestBase {
     @Description("at least one toggler element in menu")
     @DisplayName("Menu has at least one toggler element")
     void menuTogglerElementsTest() {
-        step("open https://www.apexsystems.com/", () ->
-                mainPage.openMainPage());
-
-        step("click 'accept cookies'", () ->
-                mainPage.clickAcceptCookies());
-
-        step("menu elements of class toggler are not null ", () ->
-                mainPage.togglerMenuElementsArePresent());
+                mainPage.openMainPage();
+                mainPage.clickAcceptCookies();
+                mainPage.togglerMenuElementsArePresent();
     }
 
     @Test
     @Tags({@Tag("all_tests"), @Tag("page_tests")})
-    @Description(" text assertion in footer")
+    @Description("text assertion in footer")
     @DisplayName("\"What We do\" footer contains text")
     void clickWhatWeDoTest() {
-        step("open https://www.apexsystems.com/", () ->
-                mainPage.openMainPage());
-
-        step("accept cookies", () ->
-                mainPage.clickAcceptCookies());
-
-        step("click \"What We Do\" menu item ", () ->
-                mainPage.clickWhatWeDoMenuItem());
-
-        step("footer contains \"Apex Systems is an equal opportunity employer.\" ", () ->
-                mainPage.assertFooterText());
+                mainPage.openMainPage();
+                mainPage.clickAcceptCookies();
+                mainPage.clickWhatWeDoMenuItem();
+                mainPage.assertFooterText();
     }
 
     @Test
@@ -47,11 +33,8 @@ public class ApexSystemsTests extends TestBase {
     @Description("title text assertion")
     @DisplayName("Page title should have text 'Apex Systems'")
     void titleTest() {
-        step("Open url 'https://www.apexsystems.com/'", () ->
-                mainPage.openMainPage());
-
-        step("Page title should have text 'Apex Systems'", () ->
-                mainPage.assertTitleText(title()));
+                mainPage.openMainPage();
+                mainPage.assertTitleText(title());
     }
 
     @Test
@@ -59,11 +42,8 @@ public class ApexSystemsTests extends TestBase {
     @Description("No errors in console log assertion")
     @DisplayName("Page console log should not have errors")
     void consoleShouldNotHaveErrorsTest() {
-        step("Open url 'https://www.apexsystems.com/'", () ->
-                mainPage.openMainPage());
-
-        step("Console logs should not contain text 'SEVERE'", () ->
-                mainPage.assertConsoleHaveNoErrors());
+                mainPage.openMainPage();
+                mainPage.assertConsoleHaveNoErrors();
     }
 
     @Test
@@ -71,11 +51,8 @@ public class ApexSystemsTests extends TestBase {
     @Description("console log has no warnings assertion")
     @DisplayName("Page console log should have no warnings")
     void consoleShouldNotHaveWarnings() {
-        step("Open url 'https://www.apexsystems.com/'", () ->
-                mainPage.openMainPage());
-
-        step("Console logs should not contain text 'WARNING'", () ->
-                mainPage.assertConsoleHaveNoWarnings());
+                mainPage.openMainPage();
+                mainPage.assertConsoleHaveNoWarnings();
     }
 
     @Test
